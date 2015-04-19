b5ab44309d4a691b99e00cb4ab6443ca50f5079c
Lior Uzan <sintzu@gmail.com>
2015-04-19 18:32:20 +0300

    finished activation parsing code + accuracy measuring

---
+++ activations_parsing_functions.py
@@ -0,0 +1,65 @@
+__author__ = 'lioruzan'
+import numpy as np
+import csv
+from Scientific.IO.NetCDF import *
+from scipy.stats import mode
+
+
+# @pre: csv are lists of activations of sequences, in ascending order, in CURRENNT output format.
+def parse_csv(csv_file, num_classes):
+    activations = []
+    with open(csv_file, 'r') as f:
+        for activation in csv.reader(f, delimiter=';'):
+            activation_shape = (len(activation) / num_classes, num_classes)
+            activation = activation[1:]  # remove tag, it's is sequential from 0, so not necessary when appending
+            activation = np.array(activation, 'float32').reshape(activation_shape)  # make ndarray
+            activations.append(activation)
+
+    return activations
+
+
+def get_labels_lengths_from_nc(nc_fname):
+    nc_file = NetCDFFile(nc_fname, 'r')
+    labels = nc_file.variables['targetClasses'].getValue()
+    lengths = nc_file.variables['seqLengths'].getValue()
+    nc_file.close()
+    return labels, lengths
+
+
+def get_seq_label(i, labels, lengths):
+    tgt_label_index = sum(lengths[0:i])
+    return labels[tgt_label_index]
+
+
+def get_feedforward_labels(activations):
+    num_sequences = len(activations)
+    labels = [[] for i in xrange(num_sequences)]
+    for i in xrange(num_sequences):
+
+        n_timesteps = activations[i].shape[0]
+        for j in xrange(n_timesteps):
+
+            label = np.argmax(activations[i][j])
+            labels[i].append(label)
+    return labels
+
+
+# restores original sequence classification labels from expanded timestep label list
+def restore_seq_label_list(timestep_labels, sequence_lengths):
+    l = []
+    num_sequences = len(sequence_lengths)
+    for i in xrange(num_sequences):
+        l.append(get_seq_label(i, timestep_labels, sequence_lengths))
+    return l
+
+
+def seq_labels_by_majority(timestep_labels):
+
+    num_sequences = len(timestep_labels)
+    seq_labels = np.zeros(num_sequences, 'int32')
+    for i in xrange(num_sequences):
+
+        current_label = mode(timestep_labels[i])[0][0]
+        current_label = np.int32(current_label)
+        seq_labels[i] = current_label
+    return seq_labels
--- parse_activations.py
+++ parse_activations.py
@@ -1,19 +1,34 @@
 __author__ = 'lioruzan'
-import csv
 import numpy as np
+from activations_parsing_functions import *
+from sklearn.metrics import accuracy_score
 
 
+# parameters
+num_classes = 10
+nc_file = '/home/lioruzan/bats_proj/data/nc/train_00_02.nc'
+validation_part = 2
 
-# activations of all timesteps
-csv_file = '/home/lioruzan/bats_proj/data/activations/ff_output.csv'
 
+if __name__ == '__main__':
 
-# labels of all timesteps of validation set train__00__02 for starters
+    # activations of all timesteps
+    csv_file = '/home/lioruzan/bats_proj/rnn/500_250/task00_00/test001_22.52.percent.error_ff.csv'
+    activations = parse_csv(csv_file, num_classes)
 
-# labels gotten from argmaxing softmax activations
+    # labels of sequences of validation set train_00_02 for starters
+    timestep_tru_labels, sequence_lengths = get_labels_lengths_from_nc(nc_file)
 
-# apply majority vote to decide what labels to assign validation data
+    # get timestep labels from activations
+    timestep_prediction_labels = get_feedforward_labels(activations)
 
-# calculate true accuracy
+    # calculate true accuracy
+    timestep_accuracy = accuracy_score(timestep_tru_labels, timestep_prediction_labels)
 
-# t-SNE sucka!!!!!!
+    sequence_ground_truth = restore_seq_label_list(timestep_tru_labels)
+    sequence_predictions = seq_labels_by_majority(timestep_prediction_labels)
+    sequence_accuracy = accuracy_score(sequence_ground_truth, sequence_predictions)
+
+    print('timestep accuracy: ' + str(timestep_accuracy))
+    print('sequence accuracy: ' + str(sequence_accuracy))
+    # t-SNE sucka!!!!!!
